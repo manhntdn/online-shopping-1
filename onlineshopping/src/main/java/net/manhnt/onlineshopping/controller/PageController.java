fa6c039a90ad7da5bd4d34ebfd4a1c13923b4359
Manh Nguyen <manhntdn@gmail.com>
2017-11-04 13:04:31 +0700

    manhnt commit source

--- onlineshopping/src/main/java/net/manhnt/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/manhnt/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.manhnt.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,6 +15,24 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to spring Web MVC");
 		return mv;
-		
+	}
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting){
+		if(greeting == null){
+			greeting = "Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
+	/*
+	 * @PathVariable
+	 */
+	@RequestMapping(value="/testPath/{greeting}")
+	public ModelAndView testPath(@PathVariable("greeting")String greeting){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
 	}
 }
